5fd22f988e3ed790b49bdfce787e3bf41bb82366
jeremyholbrecht <Griffley@proton.me>
2024-01-06 15:56:31 +0100

    language check greeting

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,20 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Jere")
-		want := "Hello, Chris"
+		got := Hello("Jere", "")
+		want := "Hello, Jere"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Jere", "Spanish")
+		want := "Hola, Jere"
 		assertCorrectMessage(t, got, want)
 	})
 
 }
 
-func assertCorrectMessage(t testing.TB, got, want string) {
+func assertCorrectMessage(t testing.TB, got string, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
