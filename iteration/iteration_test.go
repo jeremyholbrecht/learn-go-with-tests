ca137a57584a45a78fd4bd58c6f2e2e000a57f59
jeremyholbrecht <Griffley@proton.me>
2024-01-07 17:52:47 +0100

    refactor

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
@@ -16,6 +16,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 4)
 	}
 }
